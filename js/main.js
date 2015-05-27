22ab5b5aef9f9bf50b285ad845c3335b1508c052
Webteam SP <webmaster@sp.nl>
2015-05-27 15:27:03 +0200

    Adds context to jQuery selector to prevent duplicate js behaviour

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.SiteHeaderSearchToggle = {
     attach: function (context, settings) {
       $('.site-search').hide();
-      $('.search-toggle a').click(function(event){
+      $('.search-toggle a', context).click(function(event){
       	event.preventDefault();
       	$('.site-search').slideToggle(320);
       });
@@ -12,24 +12,24 @@
   Drupal.behaviors.SiteWaypoints = {
     attach: function(context,settings) {
   	  
-  	  // Back to top button
-  	  var wayHeader = new Waypoint({
-        element: $('.site-header')[0],
-        offset: '-52px',
-        handler: function(direction) {
-          $('.top-nav a').slideToggle("fast");
-        }
-      });
+      // Back to top button
+  	var wayHeader = new Waypoint({
+          element: $('.site-header', context)[0],
+          offset: '-52px',
+          handler: function(direction) {
+            $('.top-nav a').slideToggle("fast");
+          }
+        });
 
-  	  // Back to top button
-  	  var wayTopNav = new Waypoint({
-        element: $('.site-footer')[0],
+  	// Back to top button
+  	var wayTopNav = new Waypoint({
+        element: $('.site-footer', context)[0],
         offset: '99%',
         handler: function(direction) {
           $('.top-nav').toggleClass('bottom');
         }
       });
-      $('.top-nav').click(function(event){
+      $('.top-nav',context).click(function(event){
       	$("html, body").animate({ scrollTop: 0 }, 444);
       	return false;
       }); 
