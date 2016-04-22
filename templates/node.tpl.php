43a7a03b018a5c43e5741d83f543c9ff879ef539
Stijn Dekker <sdekker@sp.nl>
2016-04-22 13:57:26 +0200

    Removes emtpy headers from nodes (when used in panels)

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -6,8 +6,8 @@
 ?>
 <article class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <header> 
+  <?php if (!$page && $title): ?>
+    <header>
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
     </header>
   <?php endif; ?>
